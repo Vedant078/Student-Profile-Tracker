c0faea418168e89ca8e8a265c5893262bd58e4fa
vedant <ved.csbs@gmail.com>
2026-06-14 20:03:38 +0530

    Feat: Add Dept. relation in Databse

--- main.py
+++ main.py
@@ -4,11 +4,20 @@ from pydantic import  BaseModel
 from sqlmodel import Session,select,func
 from typing import Optional,List
 from database import init_db, get_session
-from models import StudentRecord, StudentUpdate
+from models import StudentRecord, StudentUpdate, Department
 
 class BranchAnalytics(BaseModel):
     branch: str
     average_cgpa: float
+
+
+class StudentProfileDetails(BaseModel):
+    student_id : int
+    student_name : str
+    cgpa : float
+    department_name : str
+    hod_name : str
+
 
 @asynccontextmanager
 async def lifeSpan(app : FastAPI):
@@ -31,6 +40,24 @@ def Welcome():
     return {"Message":"Welcome to student Profile" }
 
 #create (C)
+
+@app.post("/createDepartment", response_model=Department)
+def create_department(dept_data: Department, session: Session = Depends(get_session)):
+    # 1. Check if a department with this name already exists
+    existing_dept = session.exec(
+        select(Department).where(Department.name == dept_data.name)
+    ).first()
+    
+    if existing_dept:
+        raise HTTPException(status_code=400, detail="Department already exists")
+        
+    # 2. Add the new department to the session and save
+    session.add(dept_data)
+    session.commit()
+    session.refresh(dept_data) # This populates the generated id column
+    
+    return dept_data
+
 @app.post("/createStudent",response_model = StudentRecord,status_code = status.HTTP_201_CREATED)
 def create_student(student : StudentRecord, session:Session = Depends(get_session)):
     session.add(student)
@@ -40,15 +67,13 @@ def create_student(student : StudentRecord, session:Session = Depends(get_sessio
 
 #read (R)
 @app.get("/getStudents",response_model = list[StudentRecord])
-def get_students(name : Optional[str] = None,branch : Optional[str] = None,limit: int = 10,offset : int = 0,session : Session = Depends(get_session)):
+def get_students(name : Optional[str] = None,limit: int = 10,offset : int = 0,session : Session = Depends(get_session)):
 
     statement = select(StudentRecord)
 
     if name:
         statement = statement.where(StudentRecord.name==name)
-    if branch:
-        statement = statement.where(StudentRecord.branch == branch)
-
+   
     statement = statement.offset(offset).limit(limit)
     students = session.exec(statement).all()
 
@@ -58,13 +83,26 @@ def get_students(name : Optional[str] = None,branch : Optional[str] = None,limit
         raise HTTPException(status_code= 404 ,detail="student not found")
 
 # read specific (R)
-@app.get("/getStudent/{student_id}",response_model = StudentRecord)
+@app.get("/getStudent/{student_id}",response_model = StudentProfileDetails)
 def get_student(student_id :int, session:Session = Depends(get_session)):
-    student = session.get(StudentRecord,student_id)
-    if student:
-        return student
-    else:
-        raise HTTPException(status_code=404,detail="student not found")
+   statement = (
+       select(StudentRecord, Department)
+       .join(Department)
+       .where(StudentRecord.id ==student_id)
+   )
+   result = session.exec(statement).first()
+   if not result:
+       raise HTTPException(status_code=404, detail = "student not found")
+   else:
+       stud,dept = result #result is tupples (2 types of tupples as input)
+
+       return {
+           "student_id": stud.id,
+           "student_name": stud.name,
+           "cgpa": stud.cgpa,
+           "department_name": dept.name,
+           "hod_name" : dept.hod
+       }
     
 #update(U)
 @app.patch("/updateStudent/{student_id}",response_model = StudentRecord)
@@ -81,6 +119,22 @@ def update_student(student_id : int, student : StudentUpdate, session:Session=De
         session.commit()
         session.refresh(db_student)
         return db_student
+    
+@app.patch("/updateDepartment/{department_id}",response_model = Department)
+def update_department(department_id : int, department:Department,session:Session = Depends(get_session)):
+    db_dept = session.get(Department,department_id)
+    if not db_dept:
+        raise HTTPException(status_code=404,detail="department not found")
+    else:
+        updated = department.model_dump(exclude_unset = True)
+        for key,value in updated.items():
+            setattr(db_dept,key,value)
+        
+        session.add(db_dept)
+        session.commit()
+        session.refresh(db_dept)
+        return db_dept
+
 
 #delete (D)
 @app.delete("/delete_student/{student_id}",status_code=status.HTTP_204_NO_CONTENT)
--- models.py
+++ models.py
@@ -1,11 +1,22 @@
-from typing import Optional
-from sqlmodel import SQLModel,Field
+from typing import List,Optional
+from sqlmodel import SQLModel,Field,Relationship
+
+class Department(SQLModel, table=True):
+    id:Optional[int] = Field(default=None, primary_key = True)
+    name : str = Field(index=True, unique=True)
+    hod : str
+
+    students : List["StudentRecord"] = Relationship(back_populates = "department")
+
+
+
 
 class StudentRecord(SQLModel,table=True):
     id : Optional[int] = Field(default = None, primary_key = True)
     name : str
-    branch : str
     cgpa : float
+    department_id : Optional[int] = Field(default=None, foreign_key="department.id", ondelete = "SET NULL")
+    department : Optional[Department] = Relationship(back_populates = "students")
 
 
 class StudentUpdate(SQLModel):
